86c828969e1723ee54175c64a78d2ce17e1fa7b4
hientrung <hientrung@hotmail.com>
2023-12-28 14:16:39 +0700

    mvvm model object

---
+++ Features/Models/Count.cs
@@ -0,0 +1,10 @@
+﻿namespace Features.Models
+{
+    public class Count
+    {
+        public int Value { get; private set; }
+        public void Inc() => Value++;
+
+        public static Count Instance { get; } = new Count();
+    }
+}
