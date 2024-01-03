1c8f9154e50a4de54a443d5b63bca48a43070bc4
hientrung <hientrung@hotmail.com>
2024-01-03 12:32:55 +0700

    services

---
+++ Features/Services/AgifyService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+
+namespace Features.Services
+{
+    public class AgifyService
+    {
+        class ResModel
+        {
+            public string Name { get; set; } = "";
+            public int Age { get; set; }
+        }
+
+        private HttpClient _httpClient;
+        public AgifyService(HttpClient http)
+        {
+            _httpClient = http;
+        }
+        async public Task<int> GetAge(string name)
+        {
+            var res = await _httpClient.GetFromJsonAsync<ResModel>($"https://api.agify.io?name={name}&country_id=VN");
+            return res!.Age;
+        }
+    }
+}
